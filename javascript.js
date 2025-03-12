30510c948b1d8b892b73a2f8a8a8826cbe5fc750
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-03-12 07:04:40 -0800

    complete getHumanChoice(), increase readability by adding space to if statements.

--- javascript.js
+++ javascript.js
@@ -1,18 +1,46 @@
-function getComputerChoice() {
+/* This program runs the game Rock, Paper, Scissors in the console */
 
-    // assigns a random number to computerChoice from 0 to 1
-    let computerChoice = Math.random();
+// function getComputerChoice() { 
 
-    // used for debugging
-    console.log(computerChoice)
+//     // assigns a random number to computerChoice from 0 to 1
+//     let computerChoice = Math.random();
 
-    if (computerChoice > 0 && computerChoice <= 0.33) {
-        return "rock"
-    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
-        return "paper"
+//     // used for debugging
+//     console.log(computerChoice)
+
+//     // Try to divide into thirds to make it fair.
+//     if (computerChoice > 0 && computerChoice <= 0.33) {
+//         return "rock";
+
+//     } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
+//         return "paper";
+
+//     } else {
+//         return "scissors";
+//     }    
+// }
+
+
+function getHumanChoice() {
+
+    // store and convert input to lowercase letter to ensure consistency.
+    let humanChoice = prompt("Please enter 'r' for Rock, 'p' for Paper, or 's' for Scissors.").toLowerCase();
+
+    console.log(humanChoice);
+    
+    if (humanChoice === "r") {
+        return "rock";
+
+    } else if (humanChoice === "p") {
+        return "paper";
+
+    } else if (humanChoice === "s") {
+        return "scissors";
+
     } else {
-        return "scissors"
-    }    
+        return "Please make a valid entry.";
+    }
 }
 
-console.log(getComputerChoice())
+
+console.log(getHumanChoice())
