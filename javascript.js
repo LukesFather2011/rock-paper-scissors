485ad6465ad047b3ecc1a3b648c9325503a69223
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-03-13 06:31:57 -0800

    define playRound() set up basis for the round, ironed out bugs.

--- javascript.js
+++ javascript.js
@@ -1,48 +1,70 @@
 /* This program runs the game Rock, Paper, Scissors in the console */
 
-// function getComputerChoice() { 
+function getComputerChoice() { 
 
-//     // assigns a random number to computerChoice from 0 to 1
-//     let computerChoice = Math.random();
+    // assigns a random number to computerChoice from 0 to 1
+    let computerChoice = Math.random();
 
-//     // used for debugging
-//     console.log(computerChoice)
+    // used for debugging
+    console.log(computerChoice)
 
-//     // Try to divide into thirds to make it fair.
-//     if (computerChoice > 0 && computerChoice <= 0.33) {
-//         return "rock";
+    // Try to divide into thirds to make it fair.
+    if (computerChoice > 0 && computerChoice <= 0.33) {
+        return "rock";
 
-//     } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
-//         return "paper";
+    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
+        return "paper";
 
-//     } else {
-//         return "scissors";
-//     }    
-// }
+    } else {
+        return "scissors";
+    }    
+}
 
 
-// function getHumanChoice() {
+function getHumanChoice() {
 
-//     // store and convert input to lowercase letter to ensure consistency.
-//     let humanChoice = prompt("Please enter 'r' for Rock, 'p' for Paper, or 's' for Scissors.").toLowerCase();
+    // store and convert input to lowercase letter to ensure consistency.
+    let humanChoice = prompt("Please enter 'r' for Rock, 'p' for Paper, or 's' for Scissors.").toLowerCase();
 
-//     console.log(humanChoice);
+    console.log(humanChoice);
     
-//     if (humanChoice === "r") {
-//         return "rock";
+    if (humanChoice === "r") {
+        return "rock";
 
-//     } else if (humanChoice === "p") {
-//         return "paper";
+    } else if (humanChoice === "p") {
+        return "paper";
 
-//     } else if (humanChoice === "s") {
-//         return "scissors";
+    } else if (humanChoice === "s") {
+        return "scissors";
 
-//     } else {
-//         return "Please make a valid entry.";
-//     }
-// }
+    } else {
+        return "Please make a valid entry.";
+    }
+}
 
 
 // keep tally of the scores
 let computerScore = 0;
 let humanScore =    0; 
+
+function playRound(getHumanChoice, getComputerChoice) {
+
+    // hold the choices to use for later
+    const humanSelection    = getHumanChoice;
+    const computerSelection  = getComputerChoice;
+
+    // allows the user to see what was chosen as well as allow for debugging
+    console.log(`You picked ${humanSelection}.`);
+    console.log(`Computer picked ${computerSelection}`);
+
+    // the logic for the round
+    if (humanSelection === computerSelection) {
+        console.log(`You both picked ${humanSelection}. It's a tie.`);
+
+    } else { 
+        console.log(0);
+    }
+}
+
+// function call to play the round.
+playRound(getHumanChoice(), getComputerChoice());
