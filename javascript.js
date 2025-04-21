b37188a88c31bcab9212352ae3e9cee8b3acd771
LukesFather2011 <145923666+LukesFather2011@users.noreply.github.com>
2025-04-20 22:01:38 -0800

    Add divs w/spans to hold com & player choice
    Delete funcitons, start over to incorporate change

--- javascript.js
+++ javascript.js
@@ -5,132 +5,30 @@ const container     = document.querySelector("#container");
 const humanTotal    = document.querySelector("#playerScore");
 const computerTotal = document.querySelector('#computerScore');
 
-
-function getComputerChoice() { 
-
-    // assigns a random number to computerChoice from 0 to 1
-    let computerChoice = Math.random();
-
-    // Try to divide into thirds to make it fair.
-    if (computerChoice > 0 && computerChoice <= 0.33) {
-        return "rock";
-
-    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
-        return "paper";
-
-    } else {
-        return "scissors";
-    }    
-}
-
-
-let humanChoice = "";  // Needed nice addEventListener returns null.
-
-function getHumanChoice() {
-
-    const rockButton    = document.querySelector("#rockButton");
-    const paperButton   = document.querySelector("#paperButton");
-    const scissorButton = document.querySelector("#scissorButton");
-    
-    rockButton.addEventListener("click", (event) => {
-        humanChoice = "rock";
-    })
-
-    paperButton.addEventListener("click", (event) => {
-        humanChoice = "paper";
-    })
-
-    scissorButton.addEventListener("click", (event) => {
-        humanChoice = "scissors";
-    })
-}
-
-
-function checkIfWin(getHumanChoice, getComputerChoice) {
-
-    // hold the choices to use for later
-    const humanSelection     = getHumanChoice;
-    const computerSelection  = getComputerChoice;
-
-    // If a tie
-    if (humanSelection === computerSelection) {
-        return "tie";
-
-    } else if (humanSelection != computerSelection) { 
-
-        // Player picks rock
-        if (humanSelection === 'rock') {
-            
-            if (computerSelection === "scissors") {     // Player wins
-                return true;
-
-            } else {                                    // Computer wins
-                return false;
-            }
-
-        // Player picks paper
-        } else if (humanSelection === 'paper') {
-
-            if (computerSelection === 'rock') {         // Player wins
-                return true;
-
-            } else {                                    // Computer Wins
-                return false;               
-            }
-
-        // Player picks scissors
-        } else if (humanSelection === 'scissors') {
-
-            if (computerSelection === 'paper') {         // Player wins
-                return true;
-
-            } else {                                    // Computer Wins
-                return false;              
-            }
-        }
-    }
-}
-
-
 // keep tally of the scores
 let computerScore = 0;
 let humanScore    = 0; 
 
-function playRound(checkIfWin) {  
+// containers to update the player and computer choices
+const playerChoice   = document.querySelector("#playerChoice");
+const computerChoice = document.querySelector("#computerChoice");
 
-    if (checkIfWin === "tie") {
-        console.log("it's a tie!");
 
-    } else if (checkIfWin) {
-        console.log("You win");
+function getComputerChoice() { 
+
+    // assigns a random number to computerChoice from 0 to 1
+    let randomPick = Math.random();
+
+    // Try to divide into thirds to make it fair.
+    if (randomPick > 0 && randomPick <= 0.33) {
+        computerChoice.textContent("Rock");
+
+    } else if (randomPick > 0.33 && randomPick <= 0.66) {
+        computerChoice.textContent("Paper");
 
     } else {
-        console.log("You Lose");
-    }
+        computerChoice.textContent("Scissors");
 
+    }    
 }
-
-
-// // Plays the game
-// function playGame() {
-
-//     //used to keep the below while loop going
-//     let gameOn = true;
-
-//     while(gameOn) {
-
-//         playRound(getHumanChoice(), getComputerChoice());
-        
-        
-//         let playAgain = prompt("Do you want to play another round? ('y' or 'n')").toLowerCase();
-
-//         if (playAgain === 'n') {
-//             gameOn = false;
-//         } 
-//     }
-// }
-
-
-// // Plays the actual game
-// playGame();
 
